2ba9d4ff7a166431339b51c4d58c9b0ac633247a
angel.garcia <angel.garcia@axisdata.net>
2024-05-27 10:40:42 +0200

    user only watch own comandas

--- app/routes/facturacion.js
+++ app/routes/facturacion.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import Constants from '../helpers/Constants';
 
 export default class FacturacionRoute extends Route {
   @service router;
@@ -9,10 +10,22 @@ export default class FacturacionRoute extends Route {
     const credential = JSON.parse(localStorage.getItem('user_credential'));
 
     if (this.login.authUser(credential)) {
-      this.comandas.initComandasList();
+      if (this.login.userType == 'Administrador') {this.comandas.initComandasList()}
+      else {this.getComandasByUserID();}
       this.router.transitionTo('/facturacion');
     } else {
       this.router.transitionTo('/login');
     }
   }
+
+  getComandasByUserID() {
+    const currentLocalComandast = JSON.parse(localStorage.getItem(Constants.COMANDAS_STORAGE));
+    const userName = JSON.parse(localStorage.getItem(Constants.USER_CREDENTIALS)).user;
+    const comandasToUpdate = currentLocalComandast.filter(
+      (item) => item.user == userName && item.status==Constants.status.A
+    );
+    this.comandas.updateComandasList(comandasToUpdate);
+
+  }
 }
+
